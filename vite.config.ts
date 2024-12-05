0199afdb4ffa8726c6146d110f8f88cc5bed4aa6
dofi4ka <dofi4ka@yandex.ru>
2024-12-05 22:30:44 +0300

    removed redundant openapi.json fix

--- vite.config.ts
+++ vite.config.ts
@@ -25,10 +25,7 @@ export default defineConfig({
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path: string) => path.replace(/^\/api/, ''),
-      },
-      '/openapi.json': {
-        target: 'http://127.0.0.1:8000',
-      },
+      }
     }
   },
   build: {
